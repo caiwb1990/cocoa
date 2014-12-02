2b8827c5c22b43e867e394ef51ab60ed766672a7
Hellc <caiwb1990@gmail.com>
2014-12-03 07:53:16 +0800

    + add prom files

---
+++ HelloWorld/HelloWorld/HELLCViewController.h
@@ -0,0 +1,13 @@
+//
+//  HELLCViewController.h
+//  HelloWorld
+//
+//  Created by wenbin.cai on 14-12-3.
+//  Copyright (c) 2014年 wenbin.cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HELLCViewController : UIViewController
+
+@end
