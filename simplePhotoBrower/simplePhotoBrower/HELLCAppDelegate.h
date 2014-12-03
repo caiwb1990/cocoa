d0bce1b3241777df329a025e338e271d082a5f57
Hellc <caiwb1990@gmail.com>
2014-12-03 08:46:10 +0800

    +add

---
+++ simplePhotoBrower/simplePhotoBrower/HELLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HELLCAppDelegate.h
+//  simplePhotoBrower
+//
+//  Created by wenbin.cai on 14-12-3.
+//  Copyright (c) 2014年 wenbin.cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HELLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ simplePhotoBrower/simplePhotoBrower/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  simplePhotoBrower
+//
+//  Created by wenbin.cai on 14-12-3.
+//  Copyright (c) 2014年 wenbin.cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HELLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HELLCAppDelegate class]));
+    }
+}
