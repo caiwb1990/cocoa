6e9ebfaaa458c72dfa550a16db482836d6f423c6
Hellc <caiwb1990@gmail.com>
2014-12-05 08:56:03 +0800

    + add a proj simple  web brewer

---
+++ simpleWebBrower/simpleWebBrower/HELLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HELLCAppDelegate.h
+//  simpleWebBrower
+//
+//  Created by wenbin.cai on 14-12-5.
+//  Copyright (c) 2014年 wenbin.cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HELLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ simpleWebBrower/simpleWebBrower/HELLCViewController.h
@@ -0,0 +1,13 @@
+//
+//  HELLCViewController.h
+//  simpleWebBrower
+//
+//  Created by wenbin.cai on 14-12-5.
+//  Copyright (c) 2014年 wenbin.cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HELLCViewController : UIViewController
+
+@end
